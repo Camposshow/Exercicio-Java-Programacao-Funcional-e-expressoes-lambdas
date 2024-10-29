c8afd6bb7402b58c9a12a21617a05b5b0d542052
Gabriel Campos <gcamposlive@gmail.com>
2024-10-29 15:40:47 -0300

    Adicionando a operação 'sorted()' e melhorando a exibição

--- src/application/Program.java
+++ src/application/Program.java
@@ -5,7 +5,6 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.text.ParseException;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 import java.util.stream.Collectors;
@@ -40,23 +39,25 @@ public class Program {
             
             employees.forEach(System.out::println); // Printando todos os funcionarios
             
-            System.out.print("Informe o salario: ");
+            System.out.print("\nInforme o salario: ");
             Double salaryUp = sc.nextDouble();
-            System.out.print("Email dos funcionarios que ganham mais que R$" + salaryUp + ": \n");
+            System.out.print("Email dos funcionarios que ganham mais que R$" + String.format("%.2f", salaryUp) + ": \n");
             
             //Pipeline
             List<String> newListEmployee = employees.stream() // Transforma a lista em uma stream
             		.filter(x -> x.getSalary() > salaryUp) // Filtra apenas os funcinarios que ganham mais que salaryUp
             		.map(x -> x.getEmail()) // Troca o conteudo da lista de funcionario para apenas os emails dos funcionarios
+            		.sorted() // Colocando em ordem alfabetica        		
             		.collect(Collectors.toList()); // Transforma a stream novamente em uma lista
-            System.out.println(Arrays.toString(newListEmployee.toArray())); // Printa o resultado do pipeline
+            
+            newListEmployee.forEach(System.out::println);
             
            //Pipeline
             Double sum = employees.stream()
             		.filter(x -> x.getName().charAt(0) == 'M') // Filtra apenas os funcionarios cujo a primera letra do nome é M
             		.map(x -> x.getSalary()) // Troca o conteudo da lista de funcinario para apenas os salarios dos funcionarios
             		.reduce(0.0, (x,y) -> x + y); // Soma os salarios
-            System.out.println("Soma do salario dos funcionarios cujo a primeira letra do nome é 'M': " + sum); // Printa o resultado do pipeline
+            System.out.println("\nSoma do salario dos funcionarios cujo a primeira letra do nome é 'M': " + String.format("R$%.2f", sum)); // Printa o resultado do pipeline
            
             		
             		
@@ -67,9 +68,7 @@ public class Program {
         
      
         
-        
 
-        
         sc.close();
     }
 
